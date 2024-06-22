727d50217b3c02ae573ce8fa690288765690dcde
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-06-22 18:32:27 +0530

    fixed navigation

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,13 +1,18 @@
 "use client";
-import { sidebarLinks } from "@/constants";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+
+import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { userId } = useAuth();
+
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -15,11 +20,14 @@ const LeftSidebar = () => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+              className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
             >
               <Image
                 src={link.imgURL}
@@ -27,11 +35,13 @@ const LeftSidebar = () => {
                 width={24}
                 height={24}
               />
+
               <p className="text-light-1 max-lg:hidden">{link.label}</p>
             </Link>
           );
         })}
       </div>
+
       <div className="mt-10 px-6">
         <SignedIn>
           <SignOutButton>
@@ -42,6 +52,7 @@ const LeftSidebar = () => {
                 width={24}
                 height={24}
               />
+
               <p className="text-light-2 max-lg:hidden">Logout</p>
             </div>
           </SignOutButton>
