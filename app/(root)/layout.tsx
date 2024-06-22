6df2c45edffa159640480b66959a08de2a533ad8
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-06-22 20:59:09 +0530

    topbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,10 +6,9 @@ import { dark } from "@clerk/themes";
 
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-
 import RightSidebar from "@/components/shared/RightSidebar";
 import Topbar from "@/components/shared/Topbar";
-import BottomBar from "@/components/shared/BottomBar";
+import BottomBar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
