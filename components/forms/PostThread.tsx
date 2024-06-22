83dfe9a7b2fcd9fb1f1d75c3cbeadefa1367bace
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-06-22 17:54:16 +0530

    implemented forms

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import * as z from "zod";
-
 import { useForm } from "react-hook-form";
-import { usePathname, useRouter } from "next/navigation";
-
+import { useOrganization } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
 
 import {
   Form,
@@ -15,29 +14,21 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
-import path from "path";
 
 interface Props {
-  user: {
-    id: string;
-    objectId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-  };
-  btnTitle: string;
+  userId: string;
 }
 
-const PostThread = ({ userId }: { userId: string }) => {
+function PostThread({ userId }: Props) {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { organization } = useOrganization();
 
   const form = useForm<z.infer<typeof ThreadValidation>>({
     resolver: zodResolver(ThreadValidation),
@@ -46,11 +37,12 @@ const PostThread = ({ userId }: { userId: string }) => {
       accountId: userId,
     },
   });
+
   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
@@ -85,5 +77,6 @@ const PostThread = ({ userId }: { userId: string }) => {
       </form>
     </Form>
   );
-};
+}
+
 export default PostThread;
